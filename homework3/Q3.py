34b8bf8d758389a9fffeb7e276b861669e2cdb45
Yury Botnikov <hornar@gmail.com>
2023-06-11 20:59:23 +0200

    corrected paths in Q3

--- homework3/Q3.py
+++ homework3/Q3.py
@@ -111,8 +111,8 @@ def train_best_model(
 
 @flow
 def main_flow_local(
-    train_path: str = "../data/green_tripdata_2023-01.parquet",
-    val_path: str = "../data/green_tripdata_2023-02.parquet",
+    train_path: str = "./data/green_tripdata_2023-01.parquet",
+    val_path: str = "./data/green_tripdata_2023-02.parquet",
 ) -> None:
     """The main training pipeline"""
 
